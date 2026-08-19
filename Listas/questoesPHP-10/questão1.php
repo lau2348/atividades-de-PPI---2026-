a0a43c04ae0ca08251747cab94f11c8caccb7cb4
Lauremylia Soares de Oliveira <20241134010031@ifrn.local>
2026-08-19 17:48:44 -0300

    as duas primeiras questoes da lista

---
+++ Listas/questoesPHP-10/questão1.php
@@ -0,0 +1,12 @@
+<?php
+// Utilizando uma estrutura de repetição while, exiba todos os números de 1 até 50.
+
+ $numero = 1; 
+while($numero <= 50) {
+    echo $numero."";
+    $numero ++;
+}
+echo "fim da contagem";
+
+
+?>
